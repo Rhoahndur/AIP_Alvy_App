afb257e39432b0cdfcb0f534bc0334cd06c02afc
rhoahndur <aleksandergaun@gmail.com>
2026-02-13 21:28:39 -0500

    copy entire tesseract

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["tesseract.js", "sharp"],
+  outputFileTracingIncludes: {
+    "/api/*": [
+      "./node_modules/tesseract.js/**/*",
+      "./node_modules/tesseract.js-core/**/*",
+    ],
+  },
 };
 
 export default nextConfig;
